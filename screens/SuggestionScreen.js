74d0baf611934120afc14e078f6789f9b9f2b2c3
mikael amghar <mikael.amghar@gmail.com>
2023-04-14 09:48:37 +0200

    searchbar modif

--- screens/SuggestionScreen.js
+++ screens/SuggestionScreen.js
@@ -288,6 +288,7 @@ const styles = StyleSheet.create({
       width: '70%',      
     },
     error:{
+      alignItems: 'center',
       fontSize: 16,
       marginTop: 10,
       color: '#FAEE1C',
