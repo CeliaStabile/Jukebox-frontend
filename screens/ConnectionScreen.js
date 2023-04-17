44762816388ac437046c31a8691bd5f28e0a55b4
Your name <Your email>
2023-04-17 16:07:53 +0200

    avant pull

--- screens/ConnectionScreen.js
+++ screens/ConnectionScreen.js
@@ -38,7 +38,7 @@ import { useNavigation } from '@react-navigation/native';
     useAuthRequest(
       {
         responseType: ResponseType.Token,
-        clientId: "db1242ce93da4429a427639434f2becd",
+        clientId: "f23691598dc1491190e048505e50122d",
         scopes: [
           "user-read-currently-playing",
           "user-read-recently-played",
@@ -53,7 +53,7 @@ import { useNavigation } from '@react-navigation/native';
         // to fetch token after authorizationEndpoint
         // this must be set to false
         usePKCE: false,
-        redirectUri: "exp://exp.host/@celias38/jukebox",
+        redirectUri: "exp://10.6.240.174:19000",
       },
       discovery
     );
